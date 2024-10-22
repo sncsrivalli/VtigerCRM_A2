aeb0f82b99bf7855af51c37eb0cf7b26551c8f04
sncsrivalli <109685225+sncsrivalli@users.noreply.github.com>
2024-10-22 15:47:17 +0530

    Update ContactInformationPage.java

--- src/main/java/objectRepositories/ContactInformationPage.java
+++ src/main/java/objectRepositories/ContactInformationPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//POM
 public class ContactInformationPage {
 
 	@FindBy(xpath = "//span[@class='dvHeaderText']")
